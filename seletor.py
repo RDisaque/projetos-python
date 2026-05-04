ee32371896479c2fb4efe68f43927807c495dd16
RDisaque <isaque.rs050609@gmail.com>
2026-05-04 14:09:00 +0000

    v2

--- seletor.py
+++ seletor.py
@@ -1,8 +1,9 @@
-#--seletor de caracteres--
-letra = input("digite a letra")
-if letra == "A":
- print("você escolheu a letra A")
-elif letra == "B":
-  print("você escolheu a letra "B")
+print("sistema seletor de caracteres 🔡")
+print("escolha uma letra e veja se ela está disponiivel")
+letra = input("digite a letra: ").lower()
+if letra == "a":
+ print("você escolheu a letra A ✅")
+elif letra == "b":
+  print("você escolheu a letra B ❌")
 else:
-  print("essa opção não está disponivel")
+  print("essa opção não está disponivel 😐")
